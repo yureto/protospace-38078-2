c8efc080505cd8b6e4eaca917c86897365130db4
yureto <yut465p@gmail.com>
2022-09-15 20:48:11 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root to: "messages#index"
+  root to: 'spaces#index'
+  devise_for :users
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create 
+  end
+  resources :users, only: :show
 end
